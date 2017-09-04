66c6d3a464eda60ac1dcef798dad8bfb4693b167
Tyler Wong <tbwong3@gmail.com>
2017-09-03 20:16:26 -0700

    Bump Glide version

--- app/src/main/java/me/tylerbwong/rebelbase/data/models/Person.kt
+++ app/src/main/java/me/tylerbwong/rebelbase/data/models/Person.kt
@@ -7,13 +7,13 @@ data class Person(val name: String, val height: String, val mass: String, val ha
                   val skinColor: String, val eyeColor: String, val birthYear: String, val gender: String) {
 
     companion object Person {
-        val NAME = "name"
-        val HEIGHT = "height"
-        val MASS = "mass"
-        val HAIR_COLOR = "hair_color"
-        val SKIN_COLOR = "skin_color"
-        val EYE_COLOR = "eye_color"
-        val BIRTH_YEAR = "birth_year"
-        val GENDER = "gender"
+        const val NAME = "name"
+        const val HEIGHT = "height"
+        const val MASS = "mass"
+        const val HAIR_COLOR = "hair_color"
+        const val SKIN_COLOR = "skin_color"
+        const val EYE_COLOR = "eye_color"
+        const val BIRTH_YEAR = "birth_year"
+        const val GENDER = "gender"
     }
 }
